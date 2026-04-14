184950938ffae52a8bdc58ad0e03bb1affc647f2
Trevor Loe <trevorgloe@gmail.com>
2026-04-14 09:31:59 -0700

    made activities for the kids

---
+++ 10A/week_3/wordsearch_exercise.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+
+/*
+NASA 42 is an open source orbital and rigid body dynamics simulation, written mainly by Eric Stoneking. The simulation uses text files to configure settings for a simulation run. Lets make a piece of code that parses a file and finds the inclination set
+*/
+
+using namespace std;
+
+int main() {
+    // this is the code to read in the file, with the exception of the while loop, all of these should be familiar
+    ifstream file("nasa42/Orb_L3.txt");
+    string str;
+    string contents;
+
+    while(getline(file, str)) {
+        contents += str;
+        contents.push_back('\n');
+    }
+    cout << contents << "\n";
+
+    // now we want find the word "Inclination" and figure out the numerical value thats in front of it
+    return 0;
+}
