7031945279925179b61e98f25624455773a80c09
Trevor <trevorgloe@gmail.com>
2026-05-19 11:35:05 -0700

    finished regression example

--- 10A/week_8/regression/fnc.cpp
+++ 10A/week_8/regression/fnc.cpp
@@ -1,99 +1,81 @@
 #include "fnc.hpp"
-#include<cmath>
-#include<iostream>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
 
-float inner_prod(vector<float>& v, vector<float>& u) {
-    float tot = 0.0;
-    if (v.size() != u.size()) {
-        throw invalid_argument("vectors are not the same size\n");
-    }
-    for (int i=0; i<v.size(); ++i) {
-        tot += v[i]*u[i];
-    }
-    return tot;
+float inner_prod(vector<float> &v, vector<float> &u) {
+  float tot = 0.0;
+  if (v.size() != u.size()) {
+    throw invalid_argument("vectors are not the same size\n");
+  }
+  for (int i = 0; i < v.size(); ++i) {
+    tot += v[i] * u[i];
+  }
+  return tot;
 }
 
-vector<float> mat_vec(vector<vector<float>>& A, vector<float>& x) {
-    vector<float> out(A.size()); // pre-allocate out
-    for (int i=0; i<A.size(); ++i) {
-        out[i] = inner_prod(A[i], x);
+vector<vector<float>> create_x_mat(vector<float> &x, int k) {
+  vector<vector<float>> A;
+  for (int i = 0; i < x.size(); ++i) {
+    vector<float> row;
+    for (int j = 0; j <= k; ++j) {
+      row.push_back(pow(x[i], j));
     }
-    return out;
+    A.push_back(row);
+  }
+  return A;
 }
 
-vector<vector<float>> create_x_mat(vector<float>& x, int k) {
-    vector<vector<float>> A;
-    for (int i=0; i< x.size(); ++i) {
-        vector<float> row;
-        for (int j=0; j<=k; ++j) {
-            row.push_back(pow(x[i], j));
-        }
-        A.push_back(row);
+vector<vector<float>> compute_AtA(vector<float> &x, int k) {
+  vector<vector<float>> A = create_x_mat(x, k);
+  vector<vector<float>> AtA;
+  for (int i = 0; i <= k; ++i) {
+    vector<float> row;
+    for (int j = 0; j <= k; ++j) {
+      vector<float> coli;
+      vector<float> colj;
+      for (int l = 0; l < x.size(); ++l) {
+        coli.push_back(A[l][i]);
+        colj.push_back(A[l][j]);
+      }
+      row.push_back(inner_prod(coli, colj));
     }
-    return A;
+    AtA.push_back(row);
+  }
+  return AtA;
 }
 
-void read_into_vecs(string s, vector<float>& x, vector<float>& y) {
-    ifstream file(s);
-    string line;
-    while (getline(file, line)) {
-        size_t space_idx = line.find(" ");
-        x.push_back(stof(line.substr(0, space_idx)));
-        y.push_back(stof(line.substr(space_idx+1, line.size()-space_idx-1)));
+vector<float> compute_regression(vector<float> &x, vector<float> &y, int k) {
+  vector<vector<float>> AtA = compute_AtA(x, k);
+  vector<vector<float>> A = create_x_mat(x, k);
+  // compute A^T y first
+  vector<float> b;
+  for (int i = 0; i <= k; ++i) {
+    vector<float> coli;
+    for (int j = 0; j < x.size(); ++j) {
+      coli.push_back(A[j][i]);
     }
-}
+    b.push_back(inner_prod(coli, y));
+  }
 
-vector<vector<float>> compute_AtA(vector<float>& x, int k) {
-    vector<vector<float>> A = create_x_mat(x, k);
-    vector<vector<float>> AtA;
-    for (int i=0; i<=k; ++i) {
-        vector<float> row;
-        for (int j=0; j<=k; ++j) {
-            vector<float> coli;
-            vector<float> colj;
-            for (int l=0; l<x.size(); ++l) {
-                coli.push_back(A[l][i]);
-                colj.push_back(A[l][j]);
-            }
-            row.push_back(inner_prod(coli, colj));
-        }
-        AtA.push_back(row);
+  // initialize x0=[0,0]
+  vector<float> m(k + 1);
+  int steps = 1000;
+  float alpha = 0.001; // step size
+  for (int p = 0; p < steps; ++p) {
+    vector<float> diff = mat_vec(AtA, m);
+    for (int i = 0; i <= k; ++i) {
+      diff[i] = alpha * (b[i] - diff[i]);
     }
-    return AtA;
-}
 
-vector<float> compute_regression(vector<float>& x, vector<float>& y, int k) {
-    vector<vector<float>> AtA = compute_AtA(x, k);
-    vector<vector<float>> A = create_x_mat(x, k);
-    // compute A^T y first
-    vector<float> b;
-    for (int i=0; i<=k; ++i) {
-        vector<float> coli;
-        for (int j=0; j<x.size(); ++j) {
-            coli.push_back(A[j][i]);
-        }
-        b.push_back(inner_prod(coli, y));
+    for (int i = 0; i <= k; ++i) {
+      m[i] += diff[i];
     }
-    
-    // initialize x0=[0,0]
-    vector<float> m(k+1);
-    int steps = 100;
-    float alpha = 0.001; // step size
-    for (int p=0; p<steps; ++p) {
-        vector<float> diff = mat_vec(AtA, m);
-        for (int i=0; i<=k; ++i) {
-            diff[i] = alpha*(b[i] - diff[i]);
-        }
-        
-        for (int i=0; i<=k; ++i) {
-            m[i] += diff[i];
-        }
-        for (int i=0; i<=k; ++i) {
-            cout << m[i] <<  " ";
-        }
-        cout << "\n";
+    for (int i = 0; i <= k; ++i) {
+      cout << m[i] << " ";
     }
-    return m;
+    cout << "\n";
+  }
+  return m;
 }
---
+++ 10A/week_8/regression/plot_data.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = []
+y = []
+with open("data.txt", "r") as f:
+    for line in f:
+        # print(line)
+        items = line.split(" ")
+        x.append(float(items[0]))
+        y.append(float(items[1]))
+
+xnp = np.array(x)
+ynp = np.array(y)
+# print(xnp)
+# print(ynp)
+fig = plt.figure()
+plt.plot(xnp, ynp, 'b.')
+
+# m1 = 0.088325
+# m2 = 0.175
+# fity = m1*xnp + m2
+
+plt.plot(xnp, fity, 'r.')
+plt.show()
